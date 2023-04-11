dd74efefd694b0fb88771cb57346440cf023763a
Daniel Arauz <daniel@Daniels-MacBook-Pro-4.local>
2023-04-11 09:42:24 +0200

    updating app's index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,54 @@
-import { Layout } from '@/components/Layout/Layout';
-import { Typography } from '@material-ui/core';
-import { NextPageWithLayout } from './_app';
-import { ReactElement } from 'react';
+import DocsLayout from "@/components/Layout/DocsLayout/DocsLayout";
+import { Layout } from "@/components/Layout/Layout";
+import { readFileSync, readdirSync } from "fs";
+import { GetStaticProps } from "next";
+import { serialize } from "next-mdx-remote/serialize";
+import { join } from "path";
+import { NextPageWithLayout } from "./_app";
 
-const Page: NextPageWithLayout = () => {
-  return (<Typography variant='h3' > Indexu</Typography>);
+const DOCS_PATH = "./docs";
+
+interface Props {
+  docs: {
+    title: string;
+    slug: string;
+    description?: string;
+    tags?: string[];
+  }[];
 }
 
-Page.getLayout = function getLayout(page: ReactElement) {
+const Page: NextPageWithLayout<Props> = ({ docs }) => {
   return (
-    <Layout>
-      {page}
-    </Layout>
+    <DocsLayout docs={docs}>
+      Hello
+    </DocsLayout>
   )
 }
 
-export default Page
+export const getStaticProps: GetStaticProps = async () => {
+  const frontMatterPromises = readdirSync(DOCS_PATH)
+    .map(async (path) => {
+      const content = readFileSync(join(DOCS_PATH, path), "utf8");
+      const mdxContent = await serialize(content, { parseFrontmatter: true });
+      return mdxContent.frontmatter;
+    });
+
+  const frontmatterSources = await Promise.all(frontMatterPromises);
+  const filtered = frontmatterSources.filter((source) => !!source?.title);
+
+  return {
+    props: {
+      docs: filtered,
+    }
+  }
+}
+
+Page.getLayout = function getLayout(page) {
+  return (
+    <Layout topMenuIndex={0} title="Welcome to the Docs">
+      {page}
+    </Layout>
+  );
+};
+
+export default Page;
